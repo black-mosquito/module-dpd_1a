3a1bc0e42896a0e099780a48edc4276fb3b2e525
bm-mailorder <63896203+black-mosquito@users.noreply.github.com>
2025-10-15 17:07:04 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,4 +1,4 @@
 <?php 
 use \Magento\Framework\Component\ComponentRegistrar;
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Blackmosquito_Nachname', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Blackmosquito_DPD_1A', __DIR__);
 ?>
